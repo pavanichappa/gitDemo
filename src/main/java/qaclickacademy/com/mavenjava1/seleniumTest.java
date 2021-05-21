93f33ab398495a83e5c6e0d368a36b35fda8fa8a
pavanichappa <pavanichappa@gmail.com>
2021-05-21 22:20:10 +0100

    first commit

---
+++ src/main/java/qaclickacademy/com/mavenjava1/seleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.com.mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	
+	@Test
+	public void browserAutomation() 
+	{
+		System.out.println("browserAutomation");
+		
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
---
+++ src/test/java/qaclickacademy/com/mavenjava1/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.com.mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeApps () 
+	{
+		System.out.println("nativeApps");
+		
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+}
